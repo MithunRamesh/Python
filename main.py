49410f665fe3d7105f7b974f25ffab636ac54220
MithunRamesh <mithunrvishnu@gmail.com>
2023-10-26 17:53:27 +0530

    first

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="My wood work",page_icon=":smiling_face_with_open_hands:",layout="wide")
+with st.container():
+    st.subheader("Hi, I am Mithun :prince:")
+    st.title("A Business enthusiast from Ambur")
+    st.write("I am passionate to be in forbes under30 list")
+    st.write("[To know more >](https://www.linkedin.com/in/mithun-ramesh-03a543260/)")
+
+
